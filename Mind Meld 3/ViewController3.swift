432cea2a1757bdcc2114b2989d60fbfcf0746b90
louiskonig <louiskonig@gmail.com>
2015-01-01 19:20:15 -0500

    made cases to break down the turn examples

--- Mind Meld 3/ViewController3.swift
+++ Mind Meld 3/ViewController3.swift
@@ -23,23 +23,42 @@ class ViewController3: UIViewController {
     @IBOutlet weak var w1: UILabel!
     @IBOutlet weak var w2: UILabel!
 
-
+    var gameTurn = 0
     
     
     @IBAction func addWord(sender: AnyObject) {
-    self.wordOne.text = self.txtBoxAdd.text
-    self.txtBoxAdd.text = ""
+    self.gameTurn = self.gameTurn + 1
+    
+        
+        switch gameTurn{
+        case 1:
+            self.wordOne.text = self.txtBoxAdd.text
+            self.txtBoxAdd.text = ""
+        
+        
+        default:
+            self.wordOne.text = "Error"
+            
+        }
+    
+    
     
     }
     
     
-    @IBAction func TurnOneComplete(sender: AnyObject) {
-    self.wordOneP2.text = "party"
-    self.indicatorOne.text = ""
-    self.w1.text = self.wordOne.text
-    self.w2.text = self.wordOneP2.text
-    self.indicatorTwo.text = ">>"
+    @IBAction func turnComplete(sender: AnyObject) {
+        switch self.gameTurn{
+        case 1:
+            self.wordOneP2.text = "party"
+            self.indicatorOne.text = ""
+            self.w1.text = self.wordOne.text
+            self.w2.text = self.wordOneP2.text
+            self.indicatorTwo.text = ">>"
     
+        default:
+            self.wordOneP2.text = "Error"
+        
+        }
     }
     
     
