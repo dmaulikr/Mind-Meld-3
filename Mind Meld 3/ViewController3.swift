9bed7e535ac64c4a7cf85ed7bc0d791c5d3c7159
louiskonig <louiskonig@gmail.com>
2015-01-01 19:08:23 -0500

    more game example complete

--- Mind Meld 3/ViewController3.swift
+++ Mind Meld 3/ViewController3.swift
@@ -9,21 +9,41 @@
 import UIKit
 
 class ViewController3: UIViewController {
+
+    var toPass:String!
     
     @IBOutlet weak var lblGame: UILabel!
     @IBOutlet weak var wordOne: UILabel!
+    @IBOutlet weak var wordOneP2: UILabel!
     @IBOutlet weak var txtBoxAdd: UITextField!
+    @IBOutlet weak var indicatorOne: UILabel!
+    @IBOutlet weak var indicatorTwo: UILabel!
     
+
+    @IBOutlet weak var w1: UILabel!
+    @IBOutlet weak var w2: UILabel!
+
+
     
     
     @IBAction func addWord(sender: AnyObject) {
     self.wordOne.text = self.txtBoxAdd.text
-        
+    self.txtBoxAdd.text = ""
     
     }
     
-    var toPass:String!
     
+    @IBAction func TurnOneComplete(sender: AnyObject) {
+    self.wordOneP2.text = "party"
+    self.indicatorOne.text = ""
+    self.w1.text = self.wordOne.text
+    self.w2.text = self.wordOneP2.text
+    self.indicatorTwo.text = ">>"
+    
+    }
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
